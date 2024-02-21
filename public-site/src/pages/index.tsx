958771f4a2753a4ea2f142baf9279a23e50792ad
Sergey Smolnikov <ssmol@equinor.com>
2024-02-21 16:19:47 +0100

    Style

--- public-site/src/pages/index.tsx
+++ public-site/src/pages/index.tsx
@@ -2,45 +2,43 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import './index.module.css';
+import style from './index.module.css';
+import '../css/custom.css'
 
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <div className="app">
-            <main className="home">
-                <header className="title">
-                    <h1 className="main-title">Welcome to Radix</h1>
+        <div className={style.app}>
+            <main className={style.home}>
+                <header className={style.title}>
+                    <h1 className={style.mainTitle}>Welcome to Radix</h1>
                 </header>
-                <div className="theme-default-content">
-                    <div>
-                        <div><h1 id="radix-makes-your-app-develop">
-                            Radix makes your app develop</h1>
-                        </div>
-                        <HomepageFeatures/>
+                <div className={style.title2}>
+                    <h1 className={style.title}>
+                        Radix makes your app develop</h1>
+                </div>
+                <HomepageFeatures/>
+                <div className="container">
+                    <div className="row">
                         <p>You provide your code and a Dockerfile to build
                             it, and Radix will take it from there.
                         </p>
-                        <div>
-                            <Link
-                                className="actions-buttons"
-                                to="/docs/start/">
+                        <div className={style.buttons}>
+                            <Link className={style.x}
+                                  to="/docs/start/">
                                 Get started
                             </Link>
-                            <Link
-                                className="actions-buttons"
-                                to="/docs/docs/">
+                            <Link className={style.x}
+                                  to="/docs/docs/">
                                 Read the docs
                             </Link>
-                            <Link
-                                className="actions-buttons"
-                                to="/docs/other/">
+                            <Link className={style.x}
+                                  to="/docs/community/">
                                 Our community
                             </Link>
                         </div>
                     </div>
                 </div>
-                <div id="footer"></div>
             </main>
         </div>
     );
