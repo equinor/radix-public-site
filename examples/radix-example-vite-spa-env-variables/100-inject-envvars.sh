7f17659b96f8528ca526474ad62a0cf346d440a2
Magnus Heskestad Waage <mhwa@equinor.com>
2025-03-21 16:13:39 +0100

    Remove debug printing

--- examples/radix-example-vite-spa-env-variables/100-inject-envvars.sh
+++ examples/radix-example-vite-spa-env-variables/100-inject-envvars.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 #
 
-set -ex
-
 envFile=$(ls -t /usr/share/nginx/html/assets/environmentVariables*.js | head -n1)
-cat $envFile
 envsubst < "$envFile" > /tmp/envFile
 cp /tmp/envFile "$envFile"
 rm /tmp/envFile
